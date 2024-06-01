8c2f4e4ecaee8dab6a00e78e20f1f692eb5e313e
Jamauri Hicks <JJHicks1@student.fullsail.edu>
2024-06-01 12:33:57 -0400

    FIxed all the random decimal values

--- SWE_App_Test.cpp
+++ SWE_App_Test.cpp
@@ -44,22 +44,22 @@ public:
 		Assert::AreEqual(18.0, Soluion, L"45-27 calculation failed");
 		Soluion = ProcessTester->calculate("252-273");
 		Assert::AreEqual(-21.0, Soluion, L"252-273 calculation failed");
-		Soluion = ProcessTester->calculate("2.665+3.14");
-		Assert::AreEqual(-0.475, Soluion, L"2.665-3.14 calculation failed");
+		Soluion = ProcessTester->calculate("2.665-3.14");
+		Assert::AreEqual(-0.47500000000000009, Soluion, L"2.665-3.14 calculation failed");
 	}
 	TEST_METHOD(Multiplication) {
 		double Soluion;
 		Processor* ProcessTester = Processor::GetInstance();
-		Soluion = ProcessTester->calculate("2*2");
-		Assert::AreEqual(4.0, Soluion, L"2 * 2 calculation failed");
-		Soluion = ProcessTester->calculate("1.8*23.66");
-		Assert::AreEqual(42.588, Soluion, L"1.8*23.66 calculation failed");
-		Soluion = ProcessTester->calculate("45*63.56");
-		Assert::AreEqual(2860.2, Soluion, L"45*63.56 calculation failed");
-		Soluion = ProcessTester->calculate("252*273");
-		Assert::AreEqual(68796.0, Soluion, L"252*273 calculation failed");
-		Soluion = ProcessTester->calculate("2.665*3.14");
-		Assert::AreEqual(8.3681, Soluion, L"2.665*3.14 calculation failed");
+		Soluion = ProcessTester->calculate("2.0x2.0");
+		Assert::AreEqual(4.0, Soluion, L"2 x 2 calculation failed");
+		Soluion = ProcessTester->calculate("1.8x23.66");
+		Assert::AreEqual(42.588000000000001, Soluion, L"1.8x23.66 calculation failed");
+		Soluion = ProcessTester->calculate("45x63.56");
+		Assert::AreEqual(2860.2000000000003, Soluion, L"45x63.56 calculation failed");
+		Soluion = ProcessTester->calculate("252x273");
+		Assert::AreEqual(68796.0, Soluion, L"252x273 calculation failed");
+		Soluion = ProcessTester->calculate("2.665x3.14");
+		Assert::AreEqual(8.3681000000000001, Soluion, L"2.665x3.14 calculation failed");
 	}
 	TEST_METHOD(DIVISION) {
 		double Soluion;
@@ -92,46 +92,46 @@ public:
 	TEST_METHOD(SIN) {
 		double Soluion;
 		Processor* ProcessTester = Processor::GetInstance();
-		Soluion = ProcessTester->calculate("sin 5");
-		Assert::AreEqual(-0.95892427466, Soluion, L"sin 5 calculation failed");
-		Soluion = ProcessTester->calculate("sin 10");
-		Assert::AreEqual(-0.54402111088, Soluion, L"sin 10 calculation failed");
-		Soluion = ProcessTester->calculate("sin 15");
-		Assert::AreEqual(0.65028784015, Soluion, L"sin 15 calculation failed");
-		Soluion = ProcessTester->calculate("sin 20");
-		Assert::AreEqual(0.91294525072, Soluion, L"sin 20 calculation failed");
-		Soluion = ProcessTester->calculate("sin 25");
-		Assert::AreEqual(-0.13235175009, Soluion, L"sin 25 calculation failed");
+		Soluion = ProcessTester->calculate("s 5");
+		Assert::AreEqual(-0.95892427466313845, Soluion, L"sin 5 calculation failed");
+		Soluion = ProcessTester->calculate("s 10");
+		Assert::AreEqual(-0.54402111088936977, Soluion, L"sin 10 calculation failed");
+		Soluion = ProcessTester->calculate("s 15");
+		Assert::AreEqual(0.65028784015711683, Soluion, L"sin 15 calculation failed");
+		Soluion = ProcessTester->calculate("s 20");
+		Assert::AreEqual(0.91294525072762767, Soluion, L"sin 20 calculation failed");
+		Soluion = ProcessTester->calculate("s 25");
+		Assert::AreEqual(-0.13235175009777303, Soluion, L"sin 25 calculation failed");
 
 	}
 	TEST_METHOD(COS) {
 		double Soluion;
 		Processor* ProcessTester = Processor::GetInstance();
-		Soluion = ProcessTester->calculate("cos 5");
-		Assert::AreEqual(-0.28366218546, Soluion, L"sin 5 calculation failed");
-		Soluion = ProcessTester->calculate("cos 10");
-		Assert::AreEqual(-0.83907152907, Soluion, L"cos 10 calculation failed");
-		Soluion = ProcessTester->calculate("cos 15");
-		Assert::AreEqual(-0.75968791285, Soluion, L"cos 15 calculation failed");
-		Soluion = ProcessTester->calculate("cos 20");
-		Assert::AreEqual(0.40808206181, Soluion, L"cos 20 calculation failed");
-		Soluion = ProcessTester->calculate("cos 25");
-		Assert::AreEqual(-0.99120281186, Soluion, L"cos 25 calculation failed");
+		Soluion = ProcessTester->calculate("c5");
+		Assert::AreEqual(0.28366218546322625, Soluion, L"cos 5 calculation failed");
+		Soluion = ProcessTester->calculate("c10");
+		Assert::AreEqual(-0.83907152907645244, Soluion, L"cos 10 calculation failed");
+		Soluion = ProcessTester->calculate("c15");
+		Assert::AreEqual(-0.75968791285882131, Soluion, L"cos 15 calculation failed");
+		Soluion = ProcessTester->calculate("c20");
+		Assert::AreEqual(0.40808206181339196, Soluion, L"cos 20 calculation failed");
+		Soluion = ProcessTester->calculate("c25");
+		Assert::AreEqual(0.99120281186347359, Soluion, L"cos 25 calculation failed");
 
 	}
 	TEST_METHOD(TAN) {
 		double Soluion;
 		Processor* ProcessTester = Processor::GetInstance();
-		Soluion = ProcessTester->calculate("tan 5");
-		Assert::AreEqual(-3.38051500625, Soluion, L"sin 5 calculation failed");
-		Soluion = ProcessTester->calculate("tan 10");
-		Assert::AreEqual(0.64836082745, Soluion, L"sin 10 calculation failed");
-		Soluion = ProcessTester->calculate("tan 15");
-		Assert::AreEqual(-0.8559934009, Soluion, L"sin 15 calculation failed");
-		Soluion = ProcessTester->calculate("tan 20");
-		Assert::AreEqual(2.23716094422, Soluion, L"sin 20 calculation failed");
-		Soluion = ProcessTester->calculate("tan 25");
-		Assert::AreEqual(-0.13352640702, Soluion, L"sin 25 calculation failed");
+		Soluion = ProcessTester->calculate("t 5");
+		Assert::AreEqual(-3.3805150062465859, Soluion, L"sin 5 calculation failed");
+		Soluion = ProcessTester->calculate("t 10");
+		Assert::AreEqual(0.64836082745908663, Soluion, L"sin 10 calculation failed");
+		Soluion = ProcessTester->calculate("t 15");
+		Assert::AreEqual(-0.85599340090851883, Soluion, L"sin 15 calculation failed");
+		Soluion = ProcessTester->calculate("t 20");
+		Assert::AreEqual(2.2371609442247422, Soluion, L"sin 20 calculation failed");
+		Soluion = ProcessTester->calculate("t 25");
+		Assert::AreEqual(-0.13352640702153587, Soluion, L"sin 25 calculation failed");
 
 	}
 
@@ -155,48 +155,66 @@ public:
 				ButtonFactory Tester;
 				wxPanel* panel = new wxPanel;
 				wxButton* btn = Tester.CreateAddition(panel, 1);
+				const char Operators = btn->GetLabel().ToStdString()[0];
+				Assert::AreEqual('+', Operators, L"Create Addition has failed");
 			}
 
 			TEST_METHOD(CREATESUBTRACT) {
 				ButtonFactory Tester;
 				wxPanel* panel = new wxPanel;
-				Tester.CreateSubtracion(panel, 1);
+				wxButton* btn = Tester.CreateSubtracion(panel, 1);
+				const char Operators = btn->GetLabel().ToStdString()[0];
+				Assert::AreEqual('-', Operators, L"Create Subtract has failed");
 			}
 			TEST_METHOD(CREATEMULTIPLY) {
 				ButtonFactory Tester;
 				wxPanel* panel = new wxPanel;
-				Tester.CreateMultiplication(panel, 1);
+				wxButton* btn = Tester.CreateMultiplication(panel, 1);
+				const char Operators = btn->GetLabel().ToStdString()[0];
+				Assert::AreEqual('*', Operators, L"Create Multiply has failed");
 			}
 			TEST_METHOD(CREATEDIVIDE) {
 				ButtonFactory Tester;
 				wxPanel* panel = new wxPanel;
-				Tester.CreateDivision(panel, 1);
+				wxButton* btn = Tester.CreateDivision(panel, 1);
+				const char Operators = btn->GetLabel().ToStdString()[0];
+				Assert::AreEqual('/', Operators, L"Create Divide has failed");
 			}
 			TEST_METHOD(CREATEMODULO) {
 				ButtonFactory Tester;
 				wxPanel* panel = new wxPanel;
-				Tester.CreateModulo(panel, 1);
+				wxButton* btn = Tester.CreateModulo(panel, 1);
+				const char Operators = btn->GetLabel().ToStdString()[0];
+				Assert::AreEqual('%', Operators, L"Create Modulo has failed");
 
 			}
 			TEST_METHOD(CREATESIN) {
 				ButtonFactory Tester;
 				wxPanel* panel = new wxPanel;
-				Tester.CreateSin(panel, 1);
+				wxButton* btn = Tester.CreateSin(panel, 1);
+				const char Operators = btn->GetLabel().ToStdString()[0];
+				Assert::AreEqual('s', Operators, L"Create sin has failed");
 			}
-			TEST_METHOD(CREATEADDITIONCOS) {
+			TEST_METHOD(CREATeCOS) {
 				ButtonFactory Tester;
 				wxPanel* panel = new wxPanel;
-				Tester.CreateCos(panel, 1);
+				wxButton* btn = Tester.CreateCos(panel, 1);
+				const char Operators = btn->GetLabel().ToStdString()[0];
+				Assert::AreEqual('c', Operators, L"Create cos has failed");
 			}
 			TEST_METHOD(CREATETAN) {
 				ButtonFactory Tester;
 				wxPanel* panel = new wxPanel;
-				Tester.CreateTan(panel, 1);
+				wxButton* btn = Tester.CreateTan(panel, 1);
+				const char Operators = btn->GetLabel().ToStdString()[0];
+				Assert::AreEqual('t', Operators, L"Create tan has failed");
 			}
 			TEST_METHOD(CREATEBUTTON) {
 				ButtonFactory Tester;
 				wxPanel* panel = new wxPanel;
-				Tester.CreateButtons("exb" , panel, 1);
+				wxButton* btn = Tester.CreateButtons("e" , panel, 1);
+				const char Operators = btn->GetLabel().ToStdString()[0];
+				Assert::AreEqual('e', Operators, L"Create Button has failed");
 			}
 
 
